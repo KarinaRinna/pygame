e0d8f1866f6be399abc407a4a74406f95e91edec
Mandarin <zurual@mail.ru>
2023-07-12 11:32:08 +0300

    добавление кнопки изменения фона

--- main.py
+++ main.py
@@ -2,21 +2,27 @@ import pygame
 
 pygame.init()
 screen = pygame.display.set_mode((600, 300)) # flags = pygame.NOFRAME
-pygame.display.set_caption('pygame_game')
+pygame.display.set_caption('pygame_game')    # имя приложения
+icon = pygame.image.load('images/icon.png')  # создаем переменную где указываем путь к файлу
+pygame.display.set_icon(icon)                # вписываем переменную
+
 
 running = True
 while running:
 
-    pygame.display.update()
+    screen .fill(('#369e09'))   # цвет фона приложения
+
+    pygame.display.update()     # обновление экрана
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
-
-
+        elif event.type == pygame.KEYDOWN:  #если тип события = нажатие на клавиатуре
+            if event.key == pygame.K_a:     # проверяем какая клавиша нажата
+                screen.fill(('blue'))       # делаем фон синим
 
 
 
 # https://www.youtube.com/watch?v=TcRRWuQXNfU&list=PLDyJYA6aTY1mLtXrGH55paZHFjpqHdDol&index=2
-# 2:00
+# 16:00
